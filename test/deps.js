0fa6d3575727fdabde34eed4da4ca9395502bf02
plantatorbob <plantatorbob@gmail.com>
2024-09-25 09:57:29 +0300

    fix: move nanoid and sourcemap out of import map

--- test/deps.js
+++ test/deps.js
@@ -1,4 +1,4 @@
 export * from 'std/assert';
 export { resolve } from 'std/path';
 
-export { SourceMapConsumer, SourceMapGenerator } from 'sourcemap';
+export { SourceMapConsumer, SourceMapGenerator } from 'https://deno.land/x/source_map@0.7.5/mod.js';
